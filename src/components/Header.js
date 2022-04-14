ca03cc1b64d75b00fe20424829dac3e17aabc341
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-04-14 17:12:55 +0200

    appbar sticky lg

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,27 +7,31 @@ import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   return (
-    <Container>
-      <AppBar
-        position="sticky"
-        sx={{ background: "transparent", boxShadow: "none" }}
-      >
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Container>
+    <AppBar
+      position="sticky"
+      sx={{
+        background: "transparent",
+        boxShadow: "none",
+        maxWidth: "lg",
+        margin: "auto",
+        color: "black",
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
 
