8a90f41a816cbd125b5137a9ae7574eea8262ee8
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-04-21 20:02:28 +0200

    lang menu icons

--- src/components/LangButtons.js
+++ src/components/LangButtons.js
@@ -2,19 +2,43 @@ import React from "react";
 
 import i18next from "i18next";
 
-import LanguageIcon from "@mui/icons-material/Language";
-
-import { Container, IconButton } from "@mui/material";
+import { Container, Button } from "@mui/material";
 
 const LangButtons = () => {
   return (
     <Container sx={{ display: "flex", justifyContent: "center" }}>
-      <IconButton onClick={() => i18next.changeLanguage("es")}>
-        <LanguageIcon></LanguageIcon>
-      </IconButton>
-      <IconButton onClick={() => i18next.changeLanguage("en")}>
-        <LanguageIcon></LanguageIcon>
-      </IconButton>
+      <Button
+        size="small"
+        sx={{
+          color: "transparent",
+          padding: 0,
+          backgroundImage: "url(/static/img/spain.png)",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          minWidth: "35px",
+          maxHeight: "20px",
+        }}
+        onClick={() => i18next.changeLanguage("es")}
+      >
+        es
+      </Button>
+      <Button
+        size="small"
+        sx={{
+          color: "transparent",
+          padding: 0,
+          backgroundImage: "url(/static/img/britain.png)",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          minWidth: "35px",
+          maxHeight: "20px",
+        }}
+        onClick={() => i18next.changeLanguage("en")}
+      >
+        en
+      </Button>
     </Container>
   );
 };
